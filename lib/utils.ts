7bc53bb9d14699a7983869af641e82767556f391
arundaya <runsworkspace@gmail.com>
2026-01-19 15:32:51 +0000

    feat: setup shadcn and biome

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,38 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export function delayedValue<T>(value: T, delay: number): Promise<T> {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(value);
+		}, delay);
+	});
+}
+
+export function delayedValueCallback<T>(
+	value: T,
+	delay: number,
+): () => Promise<T> {
+	return () =>
+		new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(value);
+			}, delay);
+		});
+}
+
+export const formatBalance = (
+	balance: number,
+	options?: {
+		withoutCurrencySymbol?: boolean;
+	},
+) => {
+	return balance.toLocaleString("id-ID", {
+		style: options?.withoutCurrencySymbol ? "decimal" : "currency",
+		currency: "IDR",
+	});
+};
