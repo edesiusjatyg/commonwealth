07a928e0dc71f90885b5d01f20d9af11562275fd
arundaya <runsworkspace@gmail.com>
2026-01-26 11:52:48 +0000

    fix: removed unused layers/integration
    all important files have been moved to root project and adjusted

--- layers/integration/src/app/api/auth/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { AuthResponse } from '@/types';
-
-// WIP
-
-export async function POST(): Promise<NextResponse<AuthResponse>> {
-    return NextResponse.json({ message: 'Logout successful' });
-}
