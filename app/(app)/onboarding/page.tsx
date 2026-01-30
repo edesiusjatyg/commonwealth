2873310edf63941d724f405e5c440e90a729ecdd
arundaya <runsworkspace@gmail.com>
2026-01-30 16:34:32 +0000

    feat: added temporary on boarding for flow

--- app/(app)/onboarding/page.tsx
+++ app/(app)/onboarding/page.tsx
@@ -1,7 +1,12 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function OnboardingPage() {
 	return (
 		<div>
 			<h1>Onboarding</h1>
+         <p>Onboarding belum selesai, langsung lanjut aja</p>
+			<Link href="/init-wallet" className={buttonVariants()}>Set up your wallet</Link>
 		</div>
 	);
 }
